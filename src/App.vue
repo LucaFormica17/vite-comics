c23000840645782c05541923853f65fe58d98077
LucaFormica17 <145986784+LucaFormica17@users.noreply.github.com>
2024-01-09 10:03:14 +0100

    [UPDATE]

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <script>
+
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
   export default {
-      
+      components: {
+        AppHeader,
+        AppMain,
+        AppFooter
+      }
   }
 </script>
+
 <template lang="">
   <div>
-    
+    <AppHeader />
+    <AppMain />
+    <AppFooter/>
   </div>
 </template>
-<style lang="">
-  
+
+<style lang="scss">
+  @use './components/styles/generals.scss';
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    @use './styles/partials/variables.scss' as *;
+</style>
